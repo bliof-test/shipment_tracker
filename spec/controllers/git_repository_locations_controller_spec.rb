fd694842fc0514522e1ef7545bf80c08ad8b384d
Dennis Ideler <ideler.dennis@gmail.com>
2016-02-16 18:48:42 +0000

    :art: Refactor GitRepositoryLocationsController specs

--- spec/controllers/git_repository_locations_controller_spec.rb
+++ spec/controllers/git_repository_locations_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
+include Shoulda::Matchers::ActionController # FIXME: https://github.com/thoughtbot/shoulda-matchers/issues/903
+
 RSpec.describe GitRepositoryLocationsController do
   context 'when logged out' do
-    let(:git_repository_location) {
+    let(:repo_location) {
       {
         'name' => 'shipment_tracker',
         'uri' => 'https://github.com/FundingCircle/shipment_tracker.git',
@@ -10,49 +12,28 @@ RSpec.describe GitRepositoryLocationsController do
     }
 
     it { is_expected.to require_authentication_on(:get, :index) }
-    it {
-      is_expected.to require_authentication_on(
-        :post,
-        :create,
-        git_repository_location: git_repository_location)
-    }
+    it { is_expected.to require_authentication_on(:post, :create, git_repository_location: repo_location) }
   end
 
-  context 'when logged in', :logged_in do
-    before do
-      session[:current_user] = User.new
+  describe 'GET #index', :logged_in do
+    subject { get :index }
+    it { is_expected.to have_http_status(:success) }
+  end
+
+  describe 'POST #create', :logged_in do
+    before { post :create, params }
+
+    context 'when the GitRepositoryLocation is invalid' do
+      let(:params) { { git_repository_location: { name: 'app', uri: 'github.com:invalid\uri' } } }
+
+      it { is_expected.to render_template(:index) }
+      it { is_expected.to set_flash.now[:error] }
     end
 
-    describe 'GET index' do
-      it 'returns http success' do
-        get :index
-        expect(response).to have_http_status(:success)
-      end
-    end
+    context 'when the GitRepositoryLocation is valid' do
+      let(:params) { { git_repository_location: { name: 'app', uri: 'ssh://git@github.com/some/repo.git' } } }
 
-    describe 'POST create' do
-      subject { post :create, params }
-
-      context 'when the GitRepositoryLocation is invalid' do
-        let(:params) {
-          { git_repository_location: { name: 'test', uri: 'github.com:invalid\uri' } }
-        }
-
-        it { is_expected.to render_template(:index) }
-
-        it 'shows an error message' do
-          post :create, params
-          expect(flash[:error]).to be_present
-        end
-      end
-
-      context 'when the GitRepositoryLocation is valid' do
-        let(:params) {
-          { git_repository_location: { name: 'test', uri: 'ssh://git@github.com/some/repo.git' } }
-        }
-
-        it { is_expected.to redirect_to(git_repository_locations_path) }
-      end
+      it { is_expected.to redirect_to(git_repository_locations_path) }
     end
   end
 end
