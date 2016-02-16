906f481be0e61beac1e98e6e7d35d6a87c487dcc
Oleksandr Kruk <oleksandr.kruk@fundingcircle.com>
2016-02-16 11:01:22 +0000

    Fix slack client initialization

--- lib/slack/notifications.rb
+++ lib/slack/notifications.rb
@@ -1,10 +1,9 @@
 require 'slack-notifier'
 
 class SlackNotifier
-  @@notifier = Slack::Notifier.new(ENV.fetch('SLACK_WEBHOOK'))
-
   def self.send(msg, channel)
-    @@notifier.channel = channel
-    @@notifier.ping(msg, link_names: 1)
+    @notifier ||= Slack::Notifier.new(ENV.fetch('SLACK_WEBHOOK'))
+    @notifier.channel = channel
+    @notifier.ping(msg, link_names: 1)
   end
 end
