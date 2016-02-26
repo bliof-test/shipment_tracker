98ba0d0391cb422449de79460b504aa2f006b63c
Dennis Ideler <ideler.dennis@gmail.com>
2016-02-26 16:48:23 +0000

    Do not load ActionMailer, just ActiveJob
    Shipment Tracker doesn't use ActionMailer or even send emails.
    It's extra load for no benefit.
    
    If we need it in the future, simply revert this commit.
    
    We do need to explicitly require ActiveJob (for queueing jobs), which
    ActionMailer was previously requiring.

--- config/application.rb
+++ config/application.rb
@@ -3,13 +3,12 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_record/railtie'
+require 'active_job/railtie'
 require 'action_controller/railtie'
-require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# Requires gems listed in Gemfile, including any gems limited to :test, :development, or :production groups.
 Bundler.require(*Rails.groups)
 
 Dotenv.load
