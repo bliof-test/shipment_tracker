21a4900dcada72ebd62550d2a03609fa57b0d49b
Dennis Ideler <ideler.dennis@gmail.com>
2016-02-27 16:16:29 +0000

    Add tests for Payloads::Github

---
+++ spec/models/payloads/github_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+require 'payloads/github'
+
+RSpec.describe Payloads::Github do
+  describe '#head_sha' do
+    context 'with pull_request data' do
+      it 'returns sha' do
+        data = { 'pull_request' => { 'head' => { 'sha' => 'abc123' } } }
+        payload = Payloads::Github.new(data)
+
+        expect(payload.head_sha).to eq('abc123')
+      end
+    end
+
+    context 'with no pull_request data' do
+      it 'returns nil' do
+        payload = Payloads::Github.new('some_key' => 'some_value')
+
+        expect(payload.head_sha).to be_nil
+      end
+    end
+  end
+
+  describe '#base_repo_url' do
+    context 'with pull_request data' do
+      it 'returns html url' do
+        data = {
+          'pull_request' => {
+            'base' => {
+              'repo' => {
+                'html_url' => 'https://github.com/foo/bar',
+              },
+            },
+          },
+        }
+        payload = Payloads::Github.new(data)
+
+        expect(payload.base_repo_url).to eq('https://github.com/foo/bar')
+      end
+    end
+
+    context 'with no pull_request data' do
+      it 'returns nil' do
+        payload = Payloads::Github.new('some_key' => 'some_value')
+
+        expect(payload.base_repo_url).to be_nil
+      end
+    end
+  end
+
+  describe '#action' do
+    context 'when the payload has an action' do
+      it 'returns the action' do
+        payload = Payloads::Github.new('action' => 'opened')
+
+        expect(payload.action).to eq('opened')
+      end
+    end
+
+    context 'when the payload has no action' do
+      it 'returns nil' do
+        payload = Payloads::Github.new('some_key' => 'some_value')
+
+        expect(payload.action).to be_nil
+      end
+    end
+  end
+end
