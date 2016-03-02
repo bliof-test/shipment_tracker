17a52643356e5d2ad690572c713fc8d8d7535f0d
Dennis Ideler <dennis.ideler@fundingcircle.com>
2016-03-02 18:23:43 +0000

    Initial implementation for relinking tickets
    Signed-off-by: Oleksandr Kruk <oleksandr.kruk@fundingcircle.com>

--- app/use_cases/handle_push_event.rb
+++ app/use_cases/handle_push_event.rb
@@ -1,4 +1,6 @@
+require 'clients/jira'
 require 'git_repository_location'
+require 'repositories/ticket_repository'
 
 class HandlePushEvent
   include SolidUseCase
@@ -12,7 +14,30 @@ class HandlePushEvent
     continue(payload)
   end
 
-  def relink_tickets(args)
-    continue(args)
+  def relink_tickets(payload)
+    ticket_repo = Repositories::TicketRepository.new
+    linked_tickets = ticket_repo.tickets_for_versions(payload.before_sha)
+    return fail :no_linked_tickets if linked_tickets.empty?
+
+    linked_tickets.each do |ticket|
+      ticket.paths.each do |feature_review_path|
+        next unless feature_review_path.include?(payload.before_sha)
+        link_feature_review_to_ticket(ticket.key, feature_review_path, payload.before_sha, payload.after_sha)
+      end
+    end
+
+    continue(payload)
+  end
+
+  private
+
+  def link_feature_review_to_ticket(ticket_key, old_feature_review_path, before_sha, after_sha)
+    new_feature_review_path = old_feature_review_path.sub(before_sha, after_sha)
+    message = "[Feature ready for review|#{feature_review_url(new_feature_review_path)}]"
+    JiraClient.post_comment(ticket_key, message)
+  end
+
+  def feature_review_url(feature_review_path)
+    Rails.application.routes.url_helpers.root_url(protocol: 'https').chomp('/') + feature_review_path
   end
 end
--- spec/use_cases/handle_push_event_spec.rb
+++ spec/use_cases/handle_push_event_spec.rb
@@ -4,12 +4,17 @@ require 'handle_push_event'
 RSpec.describe HandlePushEvent do
   describe 'updating remote head' do
     it 'updates the corresponding repository location' do
-      github_payload = instance_double(Payloads::Github, full_repo_name: 'owner/repo', after_sha: 'abc123')
+      github_payload = instance_double(
+        Payloads::Github,
+        full_repo_name: 'owner/repo',
+        before_sha: 'abc123',
+        after_sha: 'def456',
+      )
 
       git_repository_location = instance_double(GitRepositoryLocation)
       allow(GitRepositoryLocation).to receive(:find_by_full_repo_name).and_return(git_repository_location)
 
-      expect(git_repository_location).to receive(:update).with(remote_head: 'abc123')
+      expect(git_repository_location).to receive(:update).with(remote_head: 'def456')
       HandlePushEvent.run(github_payload)
     end
 
@@ -24,5 +29,82 @@ RSpec.describe HandlePushEvent do
   end
 
   describe 'relinking tickets' do
+    before do
+      git_repository_location = instance_double(GitRepositoryLocation, update: nil)
+      allow(GitRepositoryLocation).to receive(:find_by_full_repo_name).and_return(git_repository_location)
+      allow(Repositories::TicketRepository).to receive(:new).and_return(ticket_repo)
+    end
+
+    let(:ticket_repo) { instance_double(Repositories::TicketRepository, tickets_for_versions: tickets) }
+
+    context 'when there are no previously linked tickets' do
+      let(:tickets) { [] }
+
+      it 'does nothing' do
+        github_payload = instance_double(
+          Payloads::Github,
+          full_repo_name: 'owner/repo',
+          before_sha: 'abc123',
+          after_sha: 'def456',
+        )
+
+        result = HandlePushEvent.run(github_payload)
+        expect(result).to be_failure
+      end
+    end
+
+    context 'when there is one previously linked ticket' do
+      let(:tickets) { [instance_double(Ticket, key: 'ISSUE-ID', paths: paths)] }
+
+      context 'with multiple Feature Reviews' do
+        context 'with one app per Feature Review' do
+          let(:paths) {
+            [
+              feature_review_path(foo: 'before'),
+              feature_review_path(bar: 'unrelated'),
+            ]
+          }
+
+          it 'posts linking comment to JIRA with relevant Feature Review' do
+            expect(JiraClient).to receive(:post_comment).once.with(
+              tickets.first.key,
+              "[Feature ready for review|#{feature_review_url(foo: 'after')}]",
+            )
+
+            github_payload = instance_double(
+              Payloads::Github,
+              full_repo_name: 'owner/foo',
+              before_sha: 'before',
+              after_sha: 'after',
+            )
+            HandlePushEvent.run(github_payload)
+          end
+        end
+
+        context 'with multiple apps per Feature Review' do
+          let(:paths) {
+            [
+              feature_review_path(foo: 'abc123', bar: 'efg324'),
+              feature_review_path(bar: 'bcd123'),
+            ]
+          }
+
+          it 'posts linking comment to JIRA with relevant Feature Review' do
+            expect(JiraClient).to receive(:post_comment).once.with(
+              tickets.first.key,
+              "[Feature ready for review|#{feature_review_url(foo: 'def456', bar: 'efg324')}]",
+            )
+
+            github_payload = instance_double(
+              Payloads::Github,
+              full_repo_name: 'owner/foo',
+              before_sha: 'abc123',
+              after_sha: 'def456',
+            )
+            HandlePushEvent.run(github_payload)
+          end
+        end
+      end
+    end
   end
 end
