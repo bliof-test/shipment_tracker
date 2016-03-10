7322d99671bb55728647a613fc089410b0548086
Oleksandr Kruk <oleksandr.kruk@fundingcircle.com>
2016-03-10 15:28:03 +0000

    Prepend hash to slack channel if needed

--- lib/slack/notifications.rb
+++ lib/slack/notifications.rb
@@ -3,7 +3,13 @@ require 'slack-notifier'
 class SlackNotifier
   def self.send(msg, channel)
     @notifier ||= Slack::Notifier.new(ENV.fetch('SLACK_WEBHOOK'))
-    @notifier.channel = channel
+    @notifier.channel prepend_hash(channel)
     @notifier.ping(msg, link_names: 1)
+  end
+
+  private
+
+  def prepend_hash(channel)
+    channel.start_with?('#') ? channel : channel.prepend('#')
   end
 end
