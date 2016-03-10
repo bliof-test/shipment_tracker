cf2f9e9188fb87fcdfa5c6dc5ab927b680615a48
Loga Jegede <loga.jegede@fundingcircle.com>
2016-03-10 16:20:22 +0000

    move Deploy Alert tests into seperate feature
    Signed-off-by: Oleksandr Kruk <oleksandr.kruk@fundingcircle.com>

--- features/step_definitions/releases_steps.rb
+++ features/step_definitions/releases_steps.rb
@@ -39,12 +39,3 @@ Then 'I should see the "$deploy_status" releases' do |deploy_status, releases_ta
   actual_releases = releases_page.public_send("#{deploy_status}_releases".to_sym)
   expect(actual_releases).to match_array(expected_releases)
 end
-
-Then 'a deploy alert should be dispatched' do
-  expect(SlackNotifier).to have_received(:send)
-    .with(
-      "GB Deploy Alert for frontend at 2016-01-22 17:34+00:00.\n"\
-      "Jeff deployed #{scenario_context.resolve_version('#merge1')}, release not authorised.",
-      'general',
-    )
-end
