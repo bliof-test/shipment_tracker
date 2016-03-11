28f1dc28eac79216102826f1befdb64ce5bb7c93
Andi Studer <andi.studer@fundingcircle.com>
2016-03-11 12:16:48 +0000

    Drop use of attachments
    - As this will be done correctly in a different story
    
    Signed-off-by: Andi Studer <andi.studer@fundingcircle.com>

--- lib/slack/notifications.rb
+++ lib/slack/notifications.rb
@@ -4,17 +4,8 @@ class SlackNotifier
   def self.send(msg, channel)
     @notifier ||= Slack::Notifier.new(ShipmentTracker::SLACK_WEBHOOK)
     @notifier.channel = prepend_hash(channel)
-    @notifier.ping(attachments: attachment_for(msg), link_names: 1)
+    @notifier.ping(msg, link_names: 1)
   end
-
-  def self.attachment_for(msg)
-    {
-      fallback: msg,
-      text: msg,
-      color: 'danger',
-    }
-  end
-  private_class_method :attachment_for
 
   def self.prepend_hash(channel)
     channel.start_with?('#') ? channel : "##{channel}"
