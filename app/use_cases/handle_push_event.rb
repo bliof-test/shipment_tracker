9c2fd6e17f33e40a07db80fd3e163b441b074277
Dennis Ideler <ideler.dennis@gmail.com>
2016-03-14 14:36:43 +0000

    Do not handle push events for annotated tags
    GitHub does not allow commit statuses to be POSTed for annotated tag
    SHAs. You would get the error:
    
        Octokit::UnprocessableEntity 422 - No commit found for SHA
    
    So we fail validation if the event is for an annotated tag.

--- app/use_cases/handle_push_event.rb
+++ app/use_cases/handle_push_event.rb
@@ -8,6 +8,7 @@ class HandlePushEvent
 
   def validate(payload)
     return fail :branch_deleted if payload.branch_deleted?
+    return fail :annotated_tag if payload.push_annotated_tag?
     return fail :repo_not_under_audit unless GitRepositoryLocation.repo_tracked?(payload.full_repo_name)
     continue(payload)
   end
