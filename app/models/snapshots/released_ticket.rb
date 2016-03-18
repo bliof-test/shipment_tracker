c29097075748605dd2f9d0bb694f970d743a0c28
Dennis Ideler <dennis.ideler@fundingcircle.com>
2016-03-18 17:11:19 +0000

    Explicit search columns
    Even though the columns specified in `against` are not used if
    `tsvector_column` is specified, it makes it more clear what is
    being searched against if we copy the columns used for the tsv
    indexes (which are specified in the DB migrations trigger).
    
    Signed-off-by: Oleksandr Kruk <oleksandr.kruk@fundingcircle.com>

--- app/models/snapshots/released_ticket.rb
+++ app/models/snapshots/released_ticket.rb
@@ -9,13 +9,13 @@ module Snapshots
     IGNORE_DOCUMENT_LENGTH = 0
 
     pg_search_scope :search_for,
-      against: { summary: 'A', description: 'D' },
+      against: { deploys: 'A', summary: 'B', description: 'D' },
       using: {
         tsearch: {
           prefix: true,
           dictionary: 'english',
           normalization: IGNORE_DOCUMENT_LENGTH,
-          any_word: true, # false is default
+          any_word: true,
           tsvector_column: 'tsv',
         },
       }
