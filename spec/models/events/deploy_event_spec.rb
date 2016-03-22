8c475fc54254c035903c70db0b00c3c7d8b444ad
Dennis Ideler <ideler.dennis@gmail.com>
2016-03-22 15:46:30 +0000

    Reorganize deploy event specs and test locale for invalid payload

--- spec/models/events/deploy_event_spec.rb
+++ spec/models/events/deploy_event_spec.rb
@@ -5,18 +5,18 @@ require 'events/deploy_event'
 RSpec.describe Events::DeployEvent do
   subject { Events::DeployEvent.new(details: payload) }
 
-  let(:payload) {
-    {
-      'app_name' => 'soMeApp',
-      'servers' => ['prod1.example.com', 'prod2.example.com'],
-      'version' => '123abc',
-      'deployed_by' => 'bob',
-      'locale' => 'us',
-      'environment' => 'staging',
+  context 'given an expected payload' do
+    let(:payload) {
+      {
+        'app_name' => 'soMeApp',
+        'servers' => ['prod1.example.com', 'prod2.example.com'],
+        'version' => '123abc',
+        'deployed_by' => 'bob',
+        'locale' => 'us',
+        'environment' => 'staging',
+      }
     }
-  }
 
-  context 'when given a valid payload' do
     it 'returns the correct values' do
       expect(subject.app_name).to eq('someapp')
       expect(subject.server).to eq('prod1.example.com')
@@ -33,71 +33,6 @@ RSpec.describe Events::DeployEvent do
 
       it 'uses default locale "gb"' do
         expect(subject.locale).to eq('gb')
-      end
-    end
-
-    context 'when the payload comes from Heroku' do
-      let(:payload) {
-        {
-          'app' => 'us-nameless-forest-uat',
-          'app_uuid' => '8d1e4aff-eac8-4ced-90c8-bf97f8334a4c',
-          'git_log' => '',
-          'head' => '2beae04',
-          'head_long' => '2beae049a22c053883661771551f914d2d3de6e5',
-          'prev_head' => '7650eb713bcaae1e4c03637eae2e333fc4fb5a74',
-          'release' => 'v189',
-          'url' => 'http://us-nameless-forest-uat.herokuapp.com',
-          'user' => 'user@example.com',
-        }
-      }
-
-      it 'returns the correct values' do
-        expect(subject.app_name).to eq('us-nameless-forest-uat')
-        expect(subject.deployed_by).to eq('user@example.com')
-        expect(subject.environment).to eq('uat')
-        expect(subject.locale).to eq('us')
-        expect(subject.server).to eq('http://us-nameless-forest-uat.herokuapp.com')
-        expect(subject.version).to eq('2beae049a22c053883661771551f914d2d3de6e5')
-      end
-
-      context 'when the app name does not have the environment in lowercase' do
-        before do
-          payload['app'] = 'nameless-forest-UAT'
-        end
-
-        it 'downcases the environment' do
-          expect(subject.environment).to eq('uat')
-        end
-      end
-
-      context 'when the app name does not have the locale prefix in lowercase' do
-        before do
-          payload['app'] = 'GB-nameless-forest'
-        end
-
-        it 'downcases the environment' do
-          expect(subject.locale).to eq('gb')
-        end
-      end
-
-      context 'when the app name does not include the environment at the end' do
-        before do
-          payload['app'] = 'nameless-forest'
-        end
-
-        it 'sets the environment to nil' do
-          expect(subject.environment).to be nil
-        end
-      end
-
-      context 'when the app name does not include the locale prefix' do
-        before do
-          payload['app'] = 'nameless-forest'
-        end
-
-        it 'sets the environment to nil' do
-          expect(subject.locale).to eq('us')
-        end
       end
     end
 
@@ -122,12 +57,73 @@ RSpec.describe Events::DeployEvent do
     end
   end
 
-  context 'when given an invalid payload' do
+  context 'given a Heroku payload' do
     let(:payload) {
       {
-        'bad' => 'news',
+        'app' => 'us-nameless-forest-uat',
+        'app_uuid' => '8d1e4aff-eac8-4ced-90c8-bf97f8334a4c',
+        'git_log' => '',
+        'head' => '2beae04',
+        'head_long' => '2beae049a22c053883661771551f914d2d3de6e5',
+        'prev_head' => '7650eb713bcaae1e4c03637eae2e333fc4fb5a74',
+        'release' => 'v189',
+        'url' => 'http://us-nameless-forest-uat.herokuapp.com',
+        'user' => 'user@example.com',
       }
     }
+
+    it 'returns the correct values' do
+      expect(subject.app_name).to eq('us-nameless-forest-uat')
+      expect(subject.deployed_by).to eq('user@example.com')
+      expect(subject.environment).to eq('uat')
+      expect(subject.locale).to eq('us')
+      expect(subject.server).to eq('http://us-nameless-forest-uat.herokuapp.com')
+      expect(subject.version).to eq('2beae049a22c053883661771551f914d2d3de6e5')
+    end
+
+    context 'when the app name does not have the environment in lowercase' do
+      before do
+        payload['app'] = 'nameless-forest-UAT'
+      end
+
+      it 'downcases the environment' do
+        expect(subject.environment).to eq('uat')
+      end
+    end
+
+    context 'when the app name does not have the locale prefix in lowercase' do
+      before do
+        payload['app'] = 'GB-nameless-forest'
+      end
+
+      it 'downcases the environment' do
+        expect(subject.locale).to eq('gb')
+      end
+    end
+
+    context 'when the app name does not include the environment at the end' do
+      before do
+        payload['app'] = 'nameless-forest'
+      end
+
+      it 'sets the environment to nil' do
+        expect(subject.environment).to be nil
+      end
+    end
+
+    context 'when the app name does not include the locale prefix' do
+      before do
+        payload['app'] = 'nameless-forest'
+      end
+
+      it 'sets the environment to nil' do
+        expect(subject.locale).to eq('us')
+      end
+    end
+  end
+
+  context 'given an unexpected payload' do
+    let(:payload) { { 'some' => 'value' } }
 
     it 'returns the correct values' do
       expect(subject.app_name).to be nil
@@ -135,6 +131,7 @@ RSpec.describe Events::DeployEvent do
       expect(subject.version).to be nil
       expect(subject.deployed_by).to be nil
       expect(subject.environment).to be nil
+      expect(subject.locale).to eq 'gb'
     end
   end
 end
