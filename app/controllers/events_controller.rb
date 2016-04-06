05939cd8f9126678411115030d7dd0093c247149
Andi Studer <andi.studer@fundingcircle.com>
2016-04-06 13:52:26 +0100

    remove event key from payload
    this seems added as a duplication by the rails controller
    
    and we do not use it

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    event_factory.create(params[:type], request.request_parameters, current_user.email)
+    event_factory.create(params[:type], request.request_parameters.except('event'), current_user.email)
 
     if redirect_path
       flash[:success] = 'Thank you for your submission. It will appear in a moment.'
