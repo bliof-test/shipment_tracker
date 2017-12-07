8576e1c7482e33fe6a6292bff74e854bd41db093
Monica Dimitrova <monica.dimitrova@fundingcircle.com>
2017-12-07 10:33:52 +0200

    Add approvers to Repositories index page

--- spec/helpers/repo_owners_helper_spec.rb
+++ spec/helpers/repo_owners_helper_spec.rb
@@ -2,29 +2,29 @@
 require 'rails_helper'
 
 RSpec.describe RepoOwnersHelper do
-  describe '#format_owner_emails' do
+  describe '#format_emails' do
     it 'works with 1 repo owner' do
       repo_owner = RepoOwner.new name: 'John', email: 'test@test.com'
 
-      expect(helper.format_owner_emails(repo_owner))
-        .to eq('<span class="text-nowrap">John &lt;test@test.com&gt;</span>')
+      expect(helper.format_emails(repo_owner))
+        .to eq('John &lt;test@test.com&gt;')
     end
 
     it 'works with array of owners' do
       repo_owner = RepoOwner.new name: 'John', email: 'test@test.com'
       repo_owner2 = RepoOwner.new email: 'test2@test.com'
 
-      expect(helper.format_owner_emails([repo_owner, repo_owner2])).to eq(
+      expect(helper.format_emails([repo_owner, repo_owner2])).to eq(
         [
-          '<span class="text-nowrap">John &lt;test@test.com&gt;</span>',
-          '<span class="text-nowrap">test2@test.com</span>',
+          'John &lt;test@test.com&gt;',
+          'test2@test.com',
         ].join(', '),
       )
     end
 
     it 'returns an empty string when nothing is provided' do
-      expect(helper.format_owner_emails).to eq('')
-      expect(helper.format_owner_emails([])).to eq('')
+      expect(helper.format_emails).to eq('')
+      expect(helper.format_emails([])).to eq('')
     end
   end
 end
