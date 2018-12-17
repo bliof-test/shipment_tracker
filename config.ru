93fa9c7fdb78569359176cae2dc58757af96ac05
Lee Smith <lee.smith@fundingcircle.com>
2018-12-17 10:29:47 +0000

    Add zero downtime deployment fix

--- config.ru
+++ config.ru
@@ -2,5 +2,9 @@
 
 # This file is used by Rack-based servers to start the application.
 
+map '/healthcheck-haproxy' do
+  run(proc { $healthcheck == 'term' ? [404, {}, ['term']] : [200, {}, ['ok']] })
+end
+
 require ::File.expand_path('../config/environment', __FILE__)
 run Rails.application
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,18 +17,13 @@ unless ENV['PROTECT_STDOUT'] == 'true'
 end
 
 before_fork do |_server, _worker|
-  Signal.trap 'TERM' do
-    Rails.logger.warn 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    Rails.logger.warn 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    $healthcheck = 'term'
   end
 
   defined?(ActiveRecord::Base) &&
