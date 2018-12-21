b62fce4c84afacd11ea4c32ead7f332d35d1cf34
Lee Smith <lee.smith@fundingcircle.com>
2018-12-21 11:15:16 +0000

    Load secrets from Vault

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
-exec envconsul-launch -prefix shipment_tracker/config \
-                          "$@"
+exec envconsul-launch \
+  -prefix shipment_tracker/config \
+  -secret-no-prefix shipment_tracker/secrets \
+  "$@"
