206453cfd2e8ebd661534c8c376615b234e5b046
Lee Smith <lee.smith@fundingcircle.com>
2019-09-11 16:51:18 +0100

    Log bad combined status response at debug level

--- lib/clients/github.rb
+++ lib/clients/github.rb
@@ -41,17 +41,18 @@ class GithubClient
   end
 
   def last_status_for(repo:, sha:)
-    statuses = client.combined_status(repo, sha)[:statuses]
+    response = client.combined_status(repo, sha)
   rescue Octokit::Error => e
     Rails.logger.warn "Failed to fetch status for #{repo} at #{sha}: #{e.class.name} #{e.message}"
     nil
   else
-    if statuses
-      statuses.reverse.find do |status|
+    if response[:statuses]
+      response[:statuses].reverse.find do |status|
         status[:context] == 'shipment-tracker'
       end
     else
       Rails.logger.warn "Failed to fetch status for #{repo} at #{sha}"
+      Rails.logger.debug "Combined status response for #{repo} at #{sha}: #{response}"
       nil
     end
   end
