e1c1df5a2021c4913a31972753f36cb635facc31
Lee Smith <LS80@users.noreply.github.com>
2019-09-17 14:18:23 +0100

    Revert "Auto-link feature reviews to JIRA tickets when GH branch name contains ticket key"

--- app/jobs/relink_ticket_job.rb
+++ app/jobs/relink_ticket_job.rb
@@ -4,9 +4,7 @@ require 'clients/jira'
 require 'honeybadger'
 require 'repositories/ticket_repository'
 
-class UpdateTicketLinksJob < ActiveJob::Base
-  JIRA_TICKET_REGEX = /(?<ticket_key>[A-Z]{2,10}-[1-9][0-9]*)/.freeze
-
+class RelinkTicketJob < ActiveJob::Base
   queue_as :default
 
   def perform(args)
@@ -14,11 +12,9 @@ class UpdateTicketLinksJob < ActiveJob::Base
     after_sha = args.delete(:after_sha)
     full_repo_name = args.delete(:full_repo_name)
     branch_created = args.delete(:branch_created)
-    branch_name = args.delete(:branch_name)
 
-    if branch_created
-      check_branch_name_for_ticket_and_link(full_repo_name, branch_name, after_sha)
-    elsif branch_name == 'master' || relink_tickets(before_sha, after_sha).empty?
+    if branch_created || commit_on_master?(full_repo_name, after_sha) ||
+       relink_tickets(before_sha, after_sha).empty?
       post_not_found_status(full_repo_name, after_sha)
     elsif @send_error_status
       post_error_status(full_repo_name, after_sha)
@@ -35,30 +31,14 @@ class UpdateTicketLinksJob < ActiveJob::Base
       ticket.paths.each do |feature_review_path|
         next unless feature_review_path.include?(before_sha)
 
-        update_path_and_link_feature_review_to_ticket(ticket.key, feature_review_path, before_sha, after_sha)
+        link_feature_review_to_ticket(ticket.key, feature_review_path, before_sha, after_sha)
       end
     end
   end
 
-  def check_branch_name_for_ticket_and_link(full_repo_name, branch_name, after_sha)
-    ticket_key = extract_ticket_key_from_branch_name(branch_name)
-
-    if ticket_key.nil?
-      post_not_found_status(full_repo_name, after_sha)
-      return
-    end
-
-    link_feature_review_to_ticket(ticket_key, url_for_repo_and_sha(full_repo_name, after_sha))
-    post_error_status(full_repo_name, after_sha) if @send_error_status
-  end
-
-  def update_path_and_link_feature_review_to_ticket(ticket_key, old_feature_review_path, before_sha, after_sha)
+  def link_feature_review_to_ticket(ticket_key, old_feature_review_path, before_sha, after_sha)
     new_feature_review_path = old_feature_review_path.sub(before_sha, after_sha)
-    link_feature_review_to_ticket(ticket_key, feature_review_url(new_feature_review_path))
-  end
-
-  def link_feature_review_to_ticket(ticket_key, url)
-    JiraClient.post_comment(ticket_key, LinkTicket.build_comment(url))
+    JiraClient.post_comment(ticket_key, LinkTicket.build_comment(feature_review_url(new_feature_review_path)))
   rescue JiraClient::InvalidKeyError
     @send_error_status = true
     Rails.logger.warn "Failed to post comment to JIRA ticket #{ticket_key}. Ticket might have been deleted."
@@ -79,12 +59,9 @@ class UpdateTicketLinksJob < ActiveJob::Base
     CommitStatus.new(full_repo_name: full_repo_name, sha: sha).error
   end
 
-  def extract_ticket_key_from_branch_name(branch_name)
-    matches = branch_name.match(JIRA_TICKET_REGEX)
-    matches[:ticket_key] if matches
-  end
+  def commit_on_master?(full_repo_name, sha)
+    git_repo = GitRepositoryLoader.from_rails_config.load(full_repo_name.split('/')[1], update_repo: true)
 
-  def url_for_repo_and_sha(full_repo_name, sha)
-    CommitStatus.new(full_repo_name: full_repo_name, sha: sha).target_url
+    git_repo.commit_on_master?(sha)
   end
 end
