972955f70cb5d886904b19277055f3cc3e6b0f4a
Sasha Gerrand <sasha.gerrand@fundingcircle.com>
2019-09-25 12:23:34 +0100

    Configure Prometheus client
    The Client class needs to be explicitly included otherwise :boom:.
    
    https://github.com/discourse/prometheus_exporter/pull/87

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,4 +1,7 @@
 if Rails.env != "test"
+  require 'prometheus_exporter/client'
+  PrometheusExporter::Client.default = PrometheusExporter::Client.new(host: ENV.fetch('PROMETHEUS_EXPORTER_HOST'))
+
   require 'prometheus_exporter/middleware'
 
   # This reports stats per request like HTTP status and timings
