60e9c31297c2dabb2747d829153e38b2d7fc90e3
Sasha Gerrand <sasha.gerrand@fundingcircle.com>
2019-09-25 12:23:34 +0100

    Configure Prometheus client
    The Client class needs to be explicitly included otherwise :boom:.
    
    https://github.com/discourse/prometheus_exporter/pull/87

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env != "test"
+  require 'prometheus_exporter/client'
   require 'prometheus_exporter/instrumentation'
   PrometheusExporter::Instrumentation::DelayedJob.register_plugin
 end
