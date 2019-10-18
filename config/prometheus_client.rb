fcb0617faeb4c8d324540c7160a57b392f503381
Lee Smith <lee.smith@fundingcircle.com>
2019-10-18 11:15:55 +0100

    Add hostname custom labels
    This allows the aggregation of the unicorn metrics across multiple
    instances.
    
    Requires the latest master branch of prometheus_exporter which includes
    https://github.com/discourse/prometheus_exporter/pull/90

--- config/prometheus_client.rb
+++ config/prometheus_client.rb
@@ -2,5 +2,8 @@
 
 require 'prometheus_exporter/client'
 
-PrometheusExporter::Client.default = PrometheusExporter::Client.new(host: ENV.fetch('PROMETHEUS_EXPORTER_HOST'),
-                                                                    port: ENV.fetch('PROMETHEUS_EXPORTER_PORT'))
+PrometheusExporter::Client.default = PrometheusExporter::Client.new(
+  host: ENV.fetch('PROMETHEUS_EXPORTER_HOST'),
+  port: ENV.fetch('PROMETHEUS_EXPORTER_PORT'),
+  custom_labels: { hostname: `hostname`.strip },
+)
