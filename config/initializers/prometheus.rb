7e3852b4742e0067d5c821effe73e2702d25b0be
Aleksandar Ivanov <aleksandar.ivanov@fundingcircle.com>
2020-01-30 15:53:36 +0200

    Refactor the prometheus initializer
    Just inline the metrics names so one can easily find them
    
    e.g. `"git_fetch_duration_seconds"` vs `"git_#{operation}_duration_seconds"`

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -6,19 +6,23 @@ unless Rails.env.test?
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 
-  %w[fetch clone].each do |operation|
-    notification = "#{operation}.git_repository_loader"
-    ActiveSupport::Notifications.subscribe(notification) do |_, start, finish, _, payload|
-      gauge = PrometheusExporter::Client.default.register(
-        :gauge,
-        "git_#{operation}_duration_seconds",
-        "The duration of a git #{operation} operation in seconds",
-      )
+  git_fetch_duration_gauge = PrometheusExporter::Client.default.register(
+    :gauge,
+    "git_fetch_duration_seconds",
+    "The duration of a git fetch operation in seconds",
+  )
 
-      gauge.observe(
-        finish - start,
-        repository: payload[:repository],
-      )
-    end
+  ActiveSupport::Notifications.subscribe("fetch.git_repository_loader") do |_, start, finish, _, payload|
+    git_fetch_duration_gauge.observe(finish - start, repository: payload[:repository])
+  end
+
+  git_clone_duration_gague = PrometheusExporter::Client.default.register(
+    :gauge,
+    "git_clone_duration_seconds",
+    "The duration of a git clone operation in seconds",
+  )
+
+  ActiveSupport::Notifications.subscribe("clone.git_repository_loader") do |_, start, finish, _, payload|
+    git_clone_duration_gague.observe(finish - start, repository: payload[:repository])
   end
 end
