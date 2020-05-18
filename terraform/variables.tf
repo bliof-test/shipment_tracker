7a3972f581219bd043c16cd0a4efb96238670513
Aleksandar Ivanov <aleksandar.ivanov@fundingcircle.com>
2020-05-18 14:04:11 +0300

    Fix the use and update terraform-module-pagerduty to the latest version
    Just specify defaults in terraform/variables.tf
    
    Fix random error:
    
    ```
      on .terraform/modules/pagerduty/main.tf line 2, in locals:
       2:   pagerduty_service_id           = var.enable_create_service ? element(coalescelist(pagerduty_service.service_constant[*].id, pagerduty_service.service_use_support_hours[*].id), 0) : ""
        |----------------
        | pagerduty_service.service_constant is empty tuple
        | pagerduty_service.service_use_support_hours is empty tuple
    ```
    
    by adding
    
    ```
      incident_urgency_rule_type = "constant"
      urgency = "low"
    ```

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
 variable "pagerduty_token" {}
-variable "escalation_policy_name" {}
-variable "service_name" {}
+
+variable "escalation_policy_name" {
+  default = "Global Platform Escalation Policy"
+}
+
+variable "service_name" {
+  default = "shipment_tracker"
+}
