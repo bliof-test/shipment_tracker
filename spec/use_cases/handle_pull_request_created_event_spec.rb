9287bcabc8ca012be522dbc252d6e48c22dfd3b2
Lee Smith <lee.smith@fundingcircle.com>
2020-11-18 13:11:29 +0000

    Fix spec for pull request created event

--- spec/use_cases/handle_pull_request_created_event_spec.rb
+++ spec/use_cases/handle_pull_request_created_event_spec.rb
@@ -5,6 +5,8 @@ require 'handle_pull_request_created_event'
 require 'payloads/github_pull_request'
 
 RSpec.describe HandlePullRequestCreatedEvent do
+  subject(:handle) { described_class.run(payload) }
+
   let(:payload_data) {
     {
       'pull_request' => {
@@ -32,6 +34,7 @@ RSpec.describe HandlePullRequestCreatedEvent do
   before do
     allow_any_instance_of(CommitStatus).to receive(:reset)
     allow(GitRepositoryLocation).to receive(:repo_tracked?).and_return(true)
+    ActiveJob::Base.queue_adapter = :test
   end
 
   describe 'validation' do
@@ -41,8 +44,7 @@ RSpec.describe HandlePullRequestCreatedEvent do
       end
 
       it 'fails' do
-        result = described_class.run(payload)
-        expect(result).to fail_with(:repo_not_under_audit)
+        expect(handle).to fail_with(:repo_not_under_audit)
       end
     end
 
@@ -50,38 +52,28 @@ RSpec.describe HandlePullRequestCreatedEvent do
       let(:base_branch) { 'other-base-branch' }
 
       it 'fails' do
-        result = described_class.run(payload)
-        expect(result).to fail_with(:base_not_master)
+        expect(handle).to fail_with(:base_not_master)
       end
     end
   end
 
-  describe 'setting commit status' do
-    let(:commit_status) { instance_double(CommitStatus, not_found: nil) }
-
-    it 'resets the GitHub commit status' do
-      allow(CommitStatus).to receive(:new).with(
-        full_repo_name: payload.full_repo_name,
-        sha: payload.after_sha,
-      ).and_return(commit_status)
-      expect(commit_status).to receive(:not_found)
-
-      described_class.run(payload)
-    end
+  it 'resets the GitHub commit status' do
+    expect {
+      handle
+    }.to have_enqueued_job(CommitStatusNotFoundJob).with(
+      full_repo_name: payload.full_repo_name,
+      sha: payload.head_sha
+    )
   end
 
-  describe 'auto-linking' do
-    it 'attempts to automatically link a Jira ticket' do
-      allow_any_instance_of(CommitStatus).to receive(:not_found)
-
-      expect(AutoLinkTicketJob).to receive(:perform_later).with(
-        repo_name: payload.repo_name,
-        head_sha: payload.head_sha,
-        branch_name: payload.branch_name,
-        title: payload.title,
-      )
-
-      described_class.run(payload)
-    end
+  it 'attempts to automatically link a Jira ticket' do
+    expect {
+      handle
+    }.to have_enqueued_job(AutoLinkTicketJob).with(
+      repo_name: payload.repo_name,
+      head_sha: payload.head_sha,
+      branch_name: payload.branch_name,
+      title: payload.title,
+    )
   end
 end
