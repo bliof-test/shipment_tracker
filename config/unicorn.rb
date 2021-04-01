abdce158a6a77f5d4453e75a49632b889f22d75d
Lee Smith <lee.smith@fundingcircle.com>
2021-04-01 22:27:52 +0100

    Try to re-enable unicorn instrumentation

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
 pid_file = './tmp/unicorn.pid'
+port = ENV.fetch('PORT_HTTP')
 
 require_relative '../lib/prometheus_client'
 
 worker_processes ENV.fetch('WEB_CONCURRENCY', 4).to_i
 
 pid pid_file
-listen ENV.fetch('PORT_HTTP')
+listen port
 
 preload_app true
 timeout ENV.fetch('UNICORN_TIMEOUT', 60).to_i
+
+PrometheusClient.instrument_unicorn(
+  pid_file: pid_file,
+  listener_address: "0.0.0.0:#{port}",
+  frequency: 10,
+)
 
 before_fork do |_server, _worker|
   defined?(ActiveRecord::Base) &&
