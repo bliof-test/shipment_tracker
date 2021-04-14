504599ec794eda9594c7063d9a838b68d210b2c3
Lee Smith <lee.smith@fundingcircle.com>
2021-04-14 18:30:21 +0100

    Update to Rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module ShipmentTracker
   # TODO: Move our constants here. Keep Rails config for actual Rails configuration.
 
   class Application < Rails::Application
+    config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers;
     # all .rb files in that directory are automatically loaded.
